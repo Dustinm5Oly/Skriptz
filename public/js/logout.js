44d0c8d71f00ae70fc80689ebbb8552b0cfba8ce
Dustinm5Oly <111804479+Dustinm5Oly@users.noreply.github.com>
2022-10-29 12:37:43 -0700

    css changes and logout js

---
+++ public/js/logout.js
@@ -0,0 +1,9 @@
+const logoutLink = document.getElementById("logout-link");
+
+logoutLink.addEventListener("click", async function(event){
+   let response = await fetch("/api/users/logout");
+   if (!response.ok) {
+    alert("Remove /logout from endpoint.")
+   }
+   return;
+})
